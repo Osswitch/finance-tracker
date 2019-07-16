8c7a2b57c7110a98add3f716bbf4acfcc766a58e
Xinyao Zhang <xyzhang0817@gmail.com>
2019-07-16 14:11:11 -0400

    Lookup Stock Details.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,12 @@
 class Stock < ApplicationRecord
+
+	def self.new_from_lookup(ticker_symbol)
+		begin
+			client = IEX::Api::Client.new(publishable_token: 'pk_d0758a13b12e41808838a49953844804')
+			looked_up_stock = client.quote(ticker_symbol)
+			new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+		rescue Exception => e
+			return nil
+		end
+	end
 end
